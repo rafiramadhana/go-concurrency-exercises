7c70a5e9b504ae3dff98fb4aea6df74bc3a7724d
rafiramadhana <rf.ramadhana@gmail.com>
2024-02-17 15:42:29 +0700

    Solve beginner:limit-service-time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,8 @@
 
 package main
 
+import "time"
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +23,17 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	c := make(chan bool)
+	go func() {
+		process()
+		c <- true
+	}()
+	select {
+	case <-c:
+		return true
+	case <-time.After(10 * time.Second):
+		return false
+	}
 }
 
 func main() {
